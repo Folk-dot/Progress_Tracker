8a4efae53ba4759340148f7fa79e2006e95a333d
Folk Krerkkiet <KrerkkietNJ@gmail.com>
2026-04-05 06:08:28 +0700

    fix/comment-out-sendFile

--- server/app.js
+++ server/app.js
@@ -23,10 +23,10 @@ app.use((err, req, res , next) => {
     res.status(err.status || 500);
     res.json({ message: err.message })
 })
-app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
+// app.use(express.static(path.join(__dirname, '../client/dist')));
+// app.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

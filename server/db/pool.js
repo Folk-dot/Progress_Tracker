5d036f43fd817f15a8c440a2c86f881c6a141c17
Folk Krerkkiet <KrerkkietNJ@gmail.com>
2026-04-05 07:57:18 +0700

    fix/try-to-fix-ENETUNREACH

--- server/db/pool.js
+++ server/db/pool.js
@@ -2,7 +2,10 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
